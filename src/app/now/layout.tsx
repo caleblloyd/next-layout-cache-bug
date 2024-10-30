6a556305e95ab18c53fe9744dca2f748f963b7c3
Caleb Lloyd <caleblloyd@gmail.com>
2024-10-30 00:36:23 -0400

    initial commit
    Signed-off-by: Caleb Lloyd <caleblloyd@gmail.com>

---
+++ src/app/now/layout.tsx
@@ -0,0 +1,22 @@
+import {ReactNode, Suspense} from "react"
+import Link from "next/link";
+
+export const dynamic = 'force-dynamic'
+
+async function LoadData(props: { children: ReactNode }) {
+  const response = await fetch(`http://localhost:3000/api`)
+  const data = await response.json() as { now: number }
+  return <>
+    {props.children}
+    <div>Got API Response at {data.now}</div>
+  </>
+}
+
+export default async function Layout(props: { children: ReactNode }) {
+  return <>
+    <div><Link href={`/`} className="text-blue-500">Home Page</Link></div>
+    <Suspense fallback={<h1>Loading... {Date.now()}</h1>}>
+      <LoadData>{props.children}</LoadData>
+    </Suspense>
+  </>
+}
---
+++ src/app/now/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <>
+    <h1>This is the Now Page</h1>
+  </>
+}
